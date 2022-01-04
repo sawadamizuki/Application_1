bdb3a108cc53ee0efa406b8d881532706de8bbe6
EC2 Default User <ec2-user@ip-172-31-37-137.ec2.internal>
2022-01-04 15:27:43 +0000

    updateのエラーメッセージから

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,6 +15,7 @@ class BooksController < ApplicationController
   def create
      @book = Book.new(book_params)
       if @book.save
+         flash[:notice] = "Book was successfully created."
          redirect_to book_path(@book)
       else
          @books = Book.all
@@ -32,6 +33,7 @@ class BooksController < ApplicationController
   def update
       @book = Book.find(params[:id])
       if @book.update(book_params)
+          flash[:notice] = "Book was successfully updated."
           redirect_to book_path(@book)
       else
          render action: :edit
